d89cd2ccca3ebbea24ff120b4971541f8a407f79
Vivek Garg <vivek.garg1407@gmail.com>
2024-07-29 14:07:29 +0530

    second commit

---
+++ backend/dist/index.js
@@ -0,0 +1,5 @@
+import express from "express";
+const app = express();
+app.
+    app.listen(5000, () => console.log("Server Open"));
+//# sourceMappingURL=index.js.map
---
+++ backend/src/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+
+
+
+app.
+app.listen(5000,()=>console.log("Server Open"));
