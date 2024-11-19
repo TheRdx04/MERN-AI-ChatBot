5719c683ade86852825d50ff3d4ce42ca4ddd55d
Vivek Garg <vivek.garg1407@gmail.com>
2024-11-19 19:15:52 +0530

    Update Backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,9 +1,10 @@
 import app from "./app.js";
-import { connectToDatabase } from "./db/connections.js";
+import { connectToDatabase } from "./db/connection.js";
+//connections and listeneres
 const PORT = process.env.PORT || 5000;
 connectToDatabase()
     .then(() => {
-    app.listen(PORT, () => console.log("Server Open & Connected to Database"));
+    app.listen(PORT, () => console.log("Server Open & Connected To Database"));
 })
     .catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import app from "./app.js";
-import { connectToDatabase } from "./db/connections.js";
+import { connectToDatabase } from "./db/connection.js";
 
+//connections and listeneres
 const PORT = process.env.PORT || 5000;
-
 connectToDatabase()
-  .then(()=>{
-    app.listen(PORT,()=>console.log("Server Open & Connected to Database"));
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log("Server Open & Connected To Database")
+    );
   })
-  .catch((err)=>console.log(err));
+  .catch((err) => console.log(err));
